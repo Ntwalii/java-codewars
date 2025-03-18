23b3a4840f497ea006da59c993e1146fdd19ba32
ntwalii <ntwaliaubin@gmail.com>
2025-03-18 17:26:45 +0200

    jaden case

---
+++ JadenCase.java
@@ -0,0 +1,19 @@
+class JadenCase {
+    public static void main(String[] args) {
+        System.out.println(toJadenCase("How can mirrors be real if our eyes aren't real"));
+    }
+
+    public static String toJadenCase(String phrase) {
+    System.out.println(phrase);
+    if (phrase == null || phrase.isEmpty()){
+      return null;
+    }
+		String[] splitPhrase = phrase.split(" ");
+    String result = "";
+    for(String word:splitPhrase){
+      result+=Character.toUpperCase(word.charAt(0))+word.substring(1,word.length())+" ";
+    }
+		return result.trim();
+	}
+
+}
